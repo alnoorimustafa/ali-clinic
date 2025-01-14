a1bb914878e13f0313e22d54edb7bf7350e07b33
Mustafa Alnoori <alnoorimustafa@gmail.com>
2025-01-14 23:02:34 +0300

    fixed form required and table columns

--- components/CreateDrug.vue
+++ components/CreateDrug.vue
@@ -6,11 +6,48 @@ const pb = new PocketBase("https://mcq-db.dakakean.com")
 
 // Reactive state
 const selected = ref([])
+const columns = ref([
+  {
+    label: "id",
+  },
+  {
+    label: "Name",
+    key: "name",
+  },
+  {
+    label: "Brand",
+    key: "brand",
+  },
+  {
+    label: "Icon",
+    key: "icon",
+  },
+  {
+    label: "Frequency",
+    key: "frequency",
+  },
+  {
+    label: "Dose",
+    key: "dose",
+  },
+  {
+    label: "When",
+    key: "when",
+  },
+  {
+    label: "Duration",
+    key: "duration",
+  },
+  {
+    label: "Note",
+    key: "note",
+  },
+])
 const form = ref({
-  icon: null,
+  icon: "",
   id: null,
-  brand: null,
   name: null,
+  brand: null,
   frequency: null,
   dose: null,
   when: null,
@@ -51,9 +88,9 @@ const fetchDrugs = async () => {
   try {
     const records = await pb.collection("drugs").getFullList()
     drugs.value = records.map((drug) => ({
-      icon: drug.icon || "",
       id: drug.id || "",
       name: drug.name || "",
+      icon: drug.icon || "",
       brand: drug.brand || "",
       frequency: drug.frequency || "",
       dose: drug.dose || "",
@@ -168,81 +205,92 @@ onMounted(fetchDrugs)
           {{ editing ? "Edit Drug" : "Add New Drug" }}
         </h1>
 
-        <form @submit.prevent="createOrUpdateDrug">
-          <div
-            v-for="(label, key) in {
-              icon: 'Icon',
-              name: 'Name',
-              brand: 'Brand',
-              frequency: 'Frequency',
-              dose: 'Dose',
-              when: 'When to Take',
-              duration: 'Duration',
-              note: 'Note',
-            }"
-            :key="key"
-            class="form-group mb-4"
+        <div
+          v-for="(label, key) in {
+            icon: 'Icon',
+            name: 'Name',
+            brand: 'Brand',
+            frequency: 'Frequency',
+            dose: 'Dose',
+            when: 'When to Take',
+            duration: 'Duration',
+            note: 'Note',
+          }"
+          :key="key"
+          class="form-group mb-4"
+        >
+          <label
+            :for="key"
+            class="block mb-2 text-sm font-medium text-gray-700"
+            >{{ label }}</label
           >
-            <label
-              :for="key"
-              class="block mb-2 text-sm font-medium text-gray-700"
-              >{{ label }}</label
-            >
-            <UTextarea
-              autoresize
-              v-if="key !== 'name' && key !== 'icon'"
-              v-model="form[key]"
-              :id="key"
-              class="textarea"
-              :placeholder="`Enter ${label.toLowerCase()} (one per line)`"
-            />
-            <UInput
-              v-else
-              v-model="form[key]"
-              :id="key"
-              type="text"
-              class="input"
-              :placeholder="`Enter ${label.toLowerCase()}`"
-              required
-            />
-          </div>
+          <UTextarea
+            :required="false"
+            autoresize
+            v-if="key !== 'name' && key !== 'icon'"
+            v-model="form[key]"
+            :id="key"
+            class="textarea"
+            :placeholder="`Enter ${label.toLowerCase()} (one per line)`"
+          />
+          <UInput
+            v-else
+            v-model="form[key]"
+            :id="key"
+            type="text"
+            class="input"
+            :placeholder="`Enter ${label.toLowerCase()}`"
+            required
+          />
+        </div>
 
-          <div v-if="errorMessage" class="text-red-500 mb-4">
-            {{ errorMessage }}
-          </div>
+        <div v-if="errorMessage" class="text-red-500 mb-4">
+          {{ errorMessage }}
+        </div>
 
-          <UButton
-            type="submit"
-            class="bg-primary mr-4"
-            :disabled="isSubmitting"
-          >
-            {{
-              isSubmitting
-                ? "Submitting..."
-                : editing
-                ? "Update Drug"
-                : "Create Drug"
-            }}
-          </UButton>
-          <UButton
-            v-if="editing"
-            type="submit"
-            class="bg-red-700"
-            :disabled="isSubmitting"
-            @click="deleteDrug"
-          >
-            Delete Drug
-          </UButton>
-        </form>
+        <UButton
+          type="submit"
+          class="bg-primary mr-4"
+          :disabled="isSubmitting"
+          @click="createOrUpdateDrug"
+        >
+          {{
+            isSubmitting
+              ? "Submitting..."
+              : editing
+              ? "Update Drug"
+              : "Create Drug"
+          }}
+        </UButton>
+        <UButton
+          v-if="editing"
+          type="submit"
+          class="bg-red-700"
+          :disabled="isSubmitting"
+          @click="deleteDrug"
+        >
+          Delete Drug
+        </UButton>
       </div>
       <div class="col-span-2">
         <UTable
           v-model="selected"
           :rows="drugs"
+          :columns="columns"
           class="w-full border rounded"
           :single-select="true"
           @select="handleTableChange"
-        />
+        >
+          <template #name-data="{ row }">
+            <span
+              :class="[
+                selected.find((drug) => drug.id === row.id) &&
+                  'text-primary-500 dark:text-primary-400',
+              ]"
+              >{{ row.name }}</span
+            >
+          </template>
+        </UTable>
       </div>
     </div>
   </UContainer>
